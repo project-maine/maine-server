5091638ef66a854cc02659d7496560bed6d2f2e3
Deng.Zixu <admin@dengzixu.com>
2021-12-24 17:11:04 +0800

    🏗️ 初始化项目结构

---
+++ maine-app-api/src/main/java/net/dengzixu/maine/MaineAppApiApplication.java
@@ -0,0 +1,13 @@
+package net.dengzixu.maine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaineAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaineAppApiApplication.class, args);
+    }
+
+}
