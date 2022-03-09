65ca561c832971cbfd11a640d1ea42b3756017cc
Deng.Zixu <admin@dengzixu.com>
2022-03-09 16:47:32 +0800

    🎨 修改类名
    * 修改考勤码 Mapper Provider 的类类名

--- maine-common-core/src/main/java/net/dengzixu/maine/mapper/provider/TaskCodeProvider.java
+++ maine-common-core/src/main/java/net/dengzixu/maine/mapper/provider/TaskCodeProvider.java
@@ -2,7 +2,7 @@ package net.dengzixu.maine.mapper.provider;
 
 import org.apache.ibatis.jdbc.SQL;
 
-public class TaskCodeSQLProvider {
+public class TaskCodeProvider {
     private static final String TABLE_NAME = "maine_attendance_task_code";
 
     private static final String[] ALL_COLUMNS = new String[]{"task_id", "code", "expire_time", "create_time", "modify_time"};
